082e6c10e7ee2ce0834cb20b6d528d6cd07a9c54
Daniel Fernando <113464341+iDANfer@users.noreply.github.com>
2023-09-04 20:19:27 -0300

    Update calcularDesconto.py

--- calcularDesconto.py
+++ calcularDesconto.py
@@ -1,19 +1,17 @@
 precoProduto = float(input("Informe o preço do produto: "))
-descontoProduto = float(input("Informe o Valor do Desconto em %:"))
-
+descontoProduto = float(input("Informe o Valor do Desconto: "))
 
 def valorDesconto(precoProduto, descontoProduto):
     descontoFinal = precoProduto * descontoProduto / 100
     return descontoFinal
-
 
 def valorFinal(precoProduto, descontoProduto):
     desconto = valorDesconto(precoProduto, descontoProduto)
     precofinal = precoProduto - desconto
     return precofinal
 
-
 resultadoDesconto = valorDesconto(precoProduto, descontoProduto)
-resultadoFinal = valorFinal(precoProduto, resultadoDesconto)
+resultadoFinal = valorFinal(precoProduto, descontoProduto)
 
+print("O Valor do Desconto é:", resultadoDesconto)
 print("O Valor Final é:", resultadoFinal)
